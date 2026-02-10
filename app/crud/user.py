5ae390953fffbcbc4b411b4f2441ea19fe2922a5
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2026-02-10 16:29:52 +0530

    made some crud user roles

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,18 +1,28 @@
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
 
-def create_user(db: session, user_in: UserCreate) -> User:
+
+def create_user(db: Session, user_in: UserCreate) -> User:
     db_user = User(
         email = user_in.email,
         hashed_password = hash_password(user_in.password),
         is_active = True
-
-        
     )
 
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def authenticate_user(db: Session, email: str, password: str) -> User | None:
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        return None
+    
+    if not verify_password(password, user.hashed_password):
+        return None
+    
+    return user
